0fd1abe66b3d13cd2dfadd73c1eec5fa5c226ae1
vfrnhy <rihs14804@gmail.com>
2022-09-28 10:40:29 +0900

    libft03

---
+++ libft-03/ft_memset.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsihmaok <rshimaok@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/14 10:30:14 by rshimaok          #+#    #+#             */
+/*   Updated: 2022/09/28 09:57:02 by rsihmaok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *ft_memset(void *str, int c, size_t n)
+{
+        unsigned char   *dest;
+
+        dest = (unsigned char *)str;
+        while (n-- > 0)
+        {
+                *dest++ = c;
+        }
+        return (str);
+}
+
+// int	main(void){
+
+// 	char buf[] = "ABCDEFGHIJK";
+// 	ft_memset(buf + 2, '1', 3);
+// 	printf("ABCDEFGHIJK→%s\n", buf);
+// 	return (0);
+// }
